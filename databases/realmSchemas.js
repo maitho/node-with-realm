763387ff5b0131fdf38b8fba84d62a3be549d1db
David M.T <thigedavidmaitho@gmail.com>
2020-08-12 00:20:54 +0300

    insert new user

--- databases/realmSchemas.js
+++ databases/realmSchemas.js
@@ -2,7 +2,7 @@
  * @author David Maitho
  * @email thigedavidmaitho@gmail.com
  * @create date 2020-08-11 19:47:40
- * @modify date 2020-08-11 19:59:43
+ * @modify date 2020-08-11 23:50:13
  * @desc [description]
  */
 
@@ -21,14 +21,25 @@
  }
 
  const databaseOptions = {
-     patth: 'RealmInNodeJS.realm',
-     scheme: [UserSchema],
+     path: 'RealmInNodeJS.realm',
+     schema: [UserSchema],
      schemaVersion: 0, //optional
  }
 
  //functions for userSchema
- const insertNewUser = newUser => Promise((resolve, reject) => {
-
+ const insertNewUser = newUser => new Promise((resolve, reject) => {
+     Realm.open(databaseOptions).then(realm => {
+         //check if user exists
+         let filteredUsers =  realm.objects(USER_SCHEMA).filtered(`name='${newUser.name.trim()}' AND email='${newUser.email.trim()}'`)
+         if(filteredUsers.length > 0) {
+             reject("User with the same name and email exists!")
+         }
+         realm.write(() => {
+             newUser.id = Math.floor(Date.now()) //create primary key value from timestamp as realm does not have auto-increment keys
+             realm.create(USER_SCHEMA, newUser)
+             resolve(newUser)
+         })
+     }).catch((error) => reject(error))
  })
 
  module.exports = {
--- express/expressApp.js
+++ express/expressApp.js
@@ -2,7 +2,7 @@
  * @author David Maitho
  * @email thigedavidmaitho@gmail.com
  * @create date 2020-08-11 00:25:07
- * @modify date 2020-08-11 19:40:53
+ * @modify date 2020-08-11 23:33:03
  * @desc [description]
  */
 
@@ -12,12 +12,41 @@
  app.use(bodyParser.urlencoded({ extended: false}))
  //parse application/json
  app.use(bodyParser.json())
+ //Databases
+ const { insertNewUser } = require('../databases/realmSchemas')
+ 
  app.get('/', (request, response) => {
      response.setHeader('Content-Type', 'application/json')
      response.send({
         status: "success",
         name: "David Maitho",
         message: "Root of nodeRealm"
+     })
+ })
+
+ //POST request to insert new user
+ app.post('/insert-new-user', (request, response) => {
+     const { tokenkey } = request.headers
+     const { name, email } = request.body
+     response.setHeader('Content-Type', 'appliication/json')
+     if(tokenkey != 'sometokenkey'){
+         response.send({
+             status: "failed",
+             message: "wrong token key!"
+         })
+         return
+     }
+     insertNewUser({ name, email }).then(insertedUser => {
+         response.send({
+             status: "success",
+             message: "New user inserted successfully",
+             data: insertedUser
+         })
+     }).catch((error) => {
+         response.send({
+             status: "failed",
+             message: `Insert User error: ${error}`
+         })
      })
  })
 
